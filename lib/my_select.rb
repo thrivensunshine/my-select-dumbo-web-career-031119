b7d3f3c2f8142af528474a5a2823f7e6006a6c3c
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-23 23:44:30 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
-def my_select(collection)
+def my_select(arr)
  # your code here!
+ i = 0
+ ans = []
+ while i < arr.length
+    if yield(arr[i])
+        ans << arr[i]
+    end
+    i += 1
+ end
+ ans
 end
+
+
+#needs more review
+# if yield 
